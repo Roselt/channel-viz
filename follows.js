8af86a0594ceab986a066d573072110c674c89db
Roselt <jaco.roselt@gmail.com>
2014-04-03 16:47:53 +0200

    Update follows.js

--- follows.js
+++ follows.js
@@ -13,7 +13,7 @@
 	*/
 
 	var defaultKey		= 'lWUrsRkbvcDvv2XsQgoR6r4zLXAcCiLxA0RjfOxtfzwLwpRQl', // Unique master Xively API key to be used as a default
-		defaultFeeds	= [1246899453,697055942], // Comma separated array of Xively Feed ID numbers
+		defaultFeeds	= [697055942], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Jaco\'s Sensor Feeds', // Replaces Xively logo in the header
 		dataDuration	= '90days', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
 		dataInterval	= 0, // Default interval for data to be displayed (in seconds)
